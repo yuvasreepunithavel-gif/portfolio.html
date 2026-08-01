5f66c9c92405576f72b04f4c86ed003a2bc97e19
yuvasreepunithavel-gif <yuvasreepunithavel@gmail.com>
2026-08-01 13:54:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#typing", {
+    strings: [
+        "AI & ML Student",
+        "Web Developer",
+        "Java Programmer",
+        "Python Learner"
+    ],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+});
